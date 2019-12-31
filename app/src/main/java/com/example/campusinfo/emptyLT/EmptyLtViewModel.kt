4bc911227873ec78c9f7e7b85b77bc09c3232e8e
Abhishek <181220003@nitdelhi.ac.in>
2019-12-31 13:09:16 +0530

    Compleated empty LT fragment and layout

--- app/src/main/java/com/example/campusinfo/emptyLT/EmptyLtViewModel.kt
+++ app/src/main/java/com/example/campusinfo/emptyLT/EmptyLtViewModel.kt
@@ -3,6 +3,8 @@ package com.example.campusinfo.emptyLT
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class EmptyLtViewModel : ViewModel() {   //NEW PROBLEM how to use file *** Solved by converting CSV file to a string resource
@@ -25,31 +27,94 @@ class EmptyLtViewModel : ViewModel() {   //NEW PROBLEM how to use file *** Solve
         }
        arrTable.add(table)
 
-   } // adds a table of 9*5 matrix in above arrTable
+   }                   // adds a table of 9*5 matrix in above arrTable
 
     fun showOutput(TimeSlot:Int,Day: Int): String{
-        var opStr="Empty LT\n"
+        var opStr="Empty LT(s)\n"
 
         var checkarr = Array (8) {1}
 
         for(t in arrTable){
-
             if(t[TimeSlot][Day]!=0)
                 checkarr[ t[TimeSlot][Day] - 1 ] = 0
-
         }
-
-
         for(k in checkarr.indices){
             if(checkarr[k]!=0){
                 opStr+="LT${k+1}\n"
             }
         }
-
-
-
         return opStr
-    }  // function which sets output textView
+    }         // function which returns values to be set in output textView
+
+    fun setCurrent():String{                                    //CONTINUE WORKING HERE use return to call show output function
+        var res = setkeys()
+        return if(res[0]==-1 && res[1]==-1) "" else showOutput(res[1],res[0])   //1 - timeSlot , 0 - day
+    }                               // used to set current time and day
+
+    private fun setkeys(): MutableList<Int> {
+        var arr: MutableList<Int>  = ArrayList()
+
+        var cal = Calendar.getInstance().time.toString()
+        Log.i("time","${cal} ${cal[11]-'0'} ${cal[12]-'0'}")
+        var flag = true
+
+        var day=cal[0].toString()+cal[1].toString()
+        if(day == "Su" || day == "Sa")
+            flag = false
+
+        var time = ( (cal[11]-'0')*10 + (cal[12]-'0') )*60 + (cal[14]-'0')*10 + (cal[15]-'0')                                    //starts at 11th index
+        if( (time < 510 || time > 1050) && day!="We"  )
+            flag = false
+
+        if( (time < 510 ||time > 930 ) && day=="We" )
+            flag = false
+
+        Log.i("values","$day $time")
+
+        if(!flag){                       // -1 indicate COLLEGE OVER
+            arr.add(-1)
+            arr.add(-1)
+        }
+        else{
+            when(day){
+                "Mo"-> arr.add(0)
+                "Tu"-> arr.add(1)
+                "We"-> arr.add(2)
+                "Th"-> arr.add(3)
+                "Fr"-> arr.add(4)
+            }
+
+            if(arr[0]==2){           //this is for Wednesday           //Continue by setting time
+                var ct=0
+                var start = 510
+                start+=60
+                if (time < start)
+                    arr.add(ct)
+                else{
+                    ct++
+                    start+=45
+                    while(time > start){
+                        ct++
+                        start+=45
+                    }
+                    arr.add(ct)
+                }
+            }
+            else{
+                var ct=0
+                var start = 510+60
+                while(time > start){
+                    ct++
+                    start+=60
+                }
+                arr.add(ct)
+            }
+        }
+
+        Log.i("date" ,"${arr[0]} ${arr[1]}")
+
+        return arr
+    }              // used to map current day and time to correct keys
 
 }
 
--- app/src/main/java/com/example/campusinfo/emptyLT/emptyLT.kt
+++ app/src/main/java/com/example/campusinfo/emptyLT/emptyLT.kt
@@ -7,15 +7,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.SpinnerAdapter
+import android.widget.*
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModel
 import com.example.campusinfo.R
 import com.example.campusinfo.databinding.EmptyLtFragmentBinding
 import kotlinx.android.synthetic.main.empty_lt_fragment.*
+import java.util.*
 
 class emptyLT : Fragment() {
 
@@ -25,38 +23,38 @@ class emptyLT : Fragment() {
     var defTimeSlot = 0             // passed in show output function timeSlot as selected timeSlot
 
     val day = arrayOf(
-        "Chose Day:",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday"
+        "   Chose Day:",
+        "   Monday",
+        "   Tuesday",
+        "   Wednesday",
+        "   Thursday",
+        "   Friday"
     )          // used to compare user i/p with wednesday to set wednesday
 
     val normalDay = arrayOf(
-        "Chose time slot:",
-        "8:30-9:30",
-        "9:30-10:30",
-        "10:30-11:30",
-        "11:30-12:30",
-        "12:30-1:30",
-        "1:30-2:30",
-        "2:30-3:30",
-        "3:30-4:30",
-        "4:30-5:30"
+        "   Chose time slot:",
+        "   8:30-9:30",
+        "   9:30-10:30",
+        "   10:30-11:30",
+        "   11:30-12:30",
+        "   12:30-1:30",
+        "   1:30-2:30",
+        "   2:30-3:30",
+        "   3:30-4:30",
+        "   4:30-5:30"
     )
 
     val wednesday = arrayOf(
-        "Chose time slot:",
-        "8:30-9:30",
-        "9:30-10:15",
-        "10:15-11:00",
-        "11:00-11:45",
-        "11:45-12:30",
-        "12:30-1:15",
-        "1:15-2:00",
-        "2:00-2:45",
-        "2:45-3:30"
+        "   Chose time slot:",
+        "   8:30-9:30",
+        "   9:30-10:15",
+        "   10:15-11:00",
+        "   11:00-11:45",
+        "   11:45-12:30",
+        "   12:30-1:15",
+        "   1:15-2:00",
+        "   2:00-2:45",
+        "   2:45-3:30"
     )
 
 
@@ -66,6 +64,7 @@ class emptyLT : Fragment() {
         val t=inflater.inflate(R.layout.empty_lt_fragment, container, false)
         val spinner_day = t.findViewById<Spinner>(R.id.spinner_day)
         val spinner_time_slot = t.findViewById<Spinner>(R.id.spinner_time_slot)
+        val button_current = t.findViewById<Button>(R.id.button_current)
 
         viewModel = ViewModelProviders.of(this).get(EmptyLtViewModel::class.java)
 
@@ -99,7 +98,7 @@ class emptyLT : Fragment() {
                // defDay = if(pos!=0) pos-1 else 0
                  defDay=pos
          //       Log.i("DaySelect","$pos")
-                if(parent!!.getItemAtPosition(pos).toString() == "Wednesday") {
+                if(parent!!.getItemAtPosition(pos).toString() == "  Wednesday") {
                     spinner_time_slot?.adapter = ArrayAdapter(
                         activity!!.applicationContext,
                         R.layout.support_simple_spinner_dropdown_item,
@@ -129,7 +128,11 @@ class emptyLT : Fragment() {
             }
         }
 
-        
+        button_current.setOnClickListener {
+            val temp = (viewModel as EmptyLtViewModel).setCurrent()
+            if(temp!="")  textView_output.text = temp
+        }
+
 
 
         return t
